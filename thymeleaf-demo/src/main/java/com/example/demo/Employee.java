ba448f67bd225edcd2219a444dce68d2c5d6eaa9
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2023-11-18 19:30:48 +0100

    First Dynamic Site

---
+++ thymeleaf-demo/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,4 @@
+package com.example.demo;
+
+public record Employee(String name, String role) {
+}
--- thymeleaf-demo/src/main/java/com/example/demo/HomeController.java
+++ thymeleaf-demo/src/main/java/com/example/demo/HomeController.java
@@ -1,10 +1,29 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private static final List<Employee> employees;
+
+    static {
+        employees = new ArrayList<>();
+        employees.add(new Employee("Frodo Baggins", "Ring Bearer"));
+        employees.add(new Employee("Bilbo Baggins", "Burglar"));
+        employees.add(new Employee("Samwise Gamgee", "Gardener"));
+    }
+
+    @GetMapping("/dynamic")
+    public String dynamic(Model model) {
+        model.addAttribute("employees", this.employees);
+        return "dynamic";
+    }
 
     @GetMapping("/")
     public String index() {
